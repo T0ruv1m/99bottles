56618c930b531100c3e371115a8db215e0eb88da
T0ruv1m <torishima.l@pronton.me>
2025-11-23 22:44:05 -0300

    .

---
+++ lib/bottles.rb
@@ -0,0 +1,39 @@
+class Bottles
+
+  def song
+    verses(99,0)
+  end
+
+  def verses(upper,lower)
+    upper.downto(lower).collect {|i| verse(i)}.join("\n")
+  end
+
+  def verse(n)
+    case n
+    when 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    else
+      "#{n} bottles of beer on the wall, " +
+      "#{n} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{n-1} #{recipient(n-1)} of beer on the wall.\n"
+    end
+  end
+
+  def recipient(n)
+    if n == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+  
+end
