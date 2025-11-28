ddc503ad61c1f2ee136b6857151a4dfe11a990bb
T0ruv1m <torishima.l@pronton.me>
2025-11-27 21:02:00 -0300

    .

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,50 +1,22 @@
+require_relative 'BottleNumber'
+
 class Bottles
 
   def song
-    verses(99,0)
+    verses(99, 0)
   end
 
-  def verses(upper,lower)
-    upper.downto(lower).collect {|i| verse(i)}.join("\n")
+  def verses(upper, lower)
+    upper.downto(lower).collect { |i| verse(i) }.join("\n")
   end
 
-  def pronoun(n=:FIXME)
-    if n == 1
-      'it'
-    else
-      'one'
-    end
-  end
+  def verse(number)
+    bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-  def quantity(n)
-    if n == 0
-      "no more"
-    else
-      n
-    end
+    "#{bottle_number} of beer on the wall, ".capitalize +
+    "#{bottle_number} of beer.\n" +
+    "#{bottle_number.action}" +
+    "#{next_bottle_number} of beer on the wall.\n"
   end
-
-  def verse(n)
-    case n
-    when 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    else
-      "#{n} #{recipient(n)} of beer on the wall, " +
-      "#{n} #{recipient(n)} of beer.\n" +
-      "Take #{pronoun(n)} down and pass it around, " +
-      "#{quantity(n-1)} #{recipient(n-1)} of beer on the wall.\n"
-    end
-  end
-
-  def recipient(n)
-    if n == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
-  
 end
