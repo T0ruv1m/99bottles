cb9fb46921646e28c44bfbd34084b4751aae3d45
T0ruv1m <torishima.l@pronton.me>
2025-11-28 21:08:57 -0300

    six-pack requirement

--- lib/BottleNumber.rb
+++ lib/BottleNumber.rb
@@ -5,47 +5,79 @@ class BottleNumber
     @number = number
   end
 
+  def self.for(number)
+    return number if number.kind_of?(BottleNumber)
+
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    when 6
+      BottleNumber6
+    else
+      BottleNumber
+    end.new(number)
+  end
+
   def to_s
     "#{quantity} #{recipient}"
   end
 
   def pronoun
-    if number == 1
-      'it'
-    else
-      'one'
-    end
+    'one'
   end
 
   def quantity
-    if number == 0
-      'no more'
-    else
-      number.to_s
-    end
+    number.to_s
   end
 
   def recipient
-    if number == 1
-      'bottle'
-    else
-      'bottles'
-    end
+    'bottles'
   end
 
   def action
-    if number == 0
-      "Go to the store and buy some more, "
-    else
-      "Take #{pronoun} down and pass it around, "
-    end
+    "Take #{pronoun} down and pass it around, "
   end
 
   def successor
-    if number == 0
-      '99'
-    else
-      number - 1
-    end
+    BottleNumber.for(number - 1)
+  end
+end
+
+class BottleNumber0 < BottleNumber
+
+  def quantity
+    'no more'
+  end
+
+  def successor
+    BottleNumber.for(99)
+  end
+
+  def action
+    "Go to the store and buy some more, "
+  end
+
+end
+
+class BottleNumber1 < BottleNumber
+
+  def pronoun
+    'it'
+  end
+
+  def recipient
+    'bottle'
+  end
+end
+
+class BottleNumber6 < BottleNumber
+  def recipient
+    'six-pack'
+  end
+
+  def quantity
+    '1'
   end
 end
